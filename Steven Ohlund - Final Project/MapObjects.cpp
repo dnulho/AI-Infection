df862f748e53e3b9b2a5b3e967fa746ed532fc3a
dnulho <nevet.dnulho@gmail.com>
2015-11-21 00:56:39 -0800

    Added connection structure in prep for creating linked list style mapgen. Started adding setter functions

--- Steven Ohlund - Final Project/MapObjects.cpp
+++ Steven Ohlund - Final Project/MapObjects.cpp
@@ -10,37 +10,73 @@
 *
 *****************************************************************/
 
+#include <random>
+#include <chrono>
+
 enum MemorySize {MB1, MB2, MB4, MB8, MB16, MB32,MB64, MB128, mMB256, MB512, MB1024, GB2, GB3, GB4, GB6, GB8, GB10, GB12, GB16, GB32, GB64, GB128, GB256, GB512, TB1 };
 enum CPUSpeed {MHz400, MHz600, MHz800, MHz1000, MHz1200, MHz1500, MHz1800, MHz2000};
 
 const int MAX_CONNECTIONS = 8;
 
+struct UniqueID
+{
+	char ID[16];
+};
+
+struct Connection
+{
+	UniqueID *connectedID;
+	bool wired;
+	unsigned short short int signalStrength;     // Niblet? only want unsigned 1-100 % range.
+	int signalSpeed;
+};
 
 class commonResources
 {
 public:
+	void TurnPowerOn();
+	void turnPowerOff();
 
 protected:
-	unsigned int simID;
+
+private:
+	bool PowerOn;
+	static UniqueID simID;
 	MemorySize RAM;
 	CPUSpeed CPU;
 	unsigned short CPU_Cores;
-	unsigned long long int HDD_Space;
-	unsigned long long int HDD_Filled;
-	bool PowerOn;
-	unsigned int cnctdTo[MAX_CONNECTIONS];
+
 };
+
 class cComputer : public commonResources
 {
-	
+	unsigned long long int HDD_Space;
+	unsigned long long int HDD_Filled;
+	Connection router;
 };
+
 class cPhone : public commonResources
 {
+	Connection router;
+
 
 };
 
 class cRouter : public commonResources
 {
-	cRouter *attachedRtrs[];
-	c
+	Connection devices[];
+	Connection routers[];
 };
+
+//////////////////////////////////////
+//         commonResources			//
+//       Function Definitions		//
+//////////////////////////////////////
+void commonResources::TurnPowerOn()
+{
+	PowerOn = true;
+}
+void commonResources::turnPowerOff()
+{
+	PowerOn = false;
+}
